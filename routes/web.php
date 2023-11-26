2766c01cb946ec659f9c3193e0bf4e5a5f64e7a9
Administrator <jiyeonyee0312@gmail.com>
2023-11-26 19:25:56 +0900

    message

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
