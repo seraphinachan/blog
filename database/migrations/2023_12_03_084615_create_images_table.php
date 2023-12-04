160a5261569d5127b85b0e46f491f1bb9b8915f7
Administrator <jiyeonyee0312@gmail.com>
2023-12-04 23:53:13 +0900

    Committing changes before merge

--- database/migrations/2023_12_03_084615_create_images_table.php
+++ database/migrations/2023_12_03_084615_create_images_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('images', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('extension');
+            $
             $table->timestamps();
         });
     }
