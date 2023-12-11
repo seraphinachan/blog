a87192571d86b1730f0da82c436660d950c87f37
Youngrong <jiyeonyee0312@gmail.com>
2023-12-11 17:52:26 +0900

    데이터 유효성 검사 및 게시물 삽입

--- app/Http/Controllers/AdminControllers/AdminPostsController.php
+++ app/Http/Controllers/AdminControllers/AdminPostsController.php
@@ -3,30 +3,136 @@
 namespace App\Http\Controllers\AdminControllers;
 
 use App\Http\Controllers\Controller;
-use App\Models\Category;
 use Illuminate\Http\Request;
+
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
 
 class AdminPostsController extends Controller
 {
+    private $rules = [
+        'title' => 'required|max:200',
+        'slug' => 'required|max:200',
+        'excerpt' => 'required|max:1000',
+        'category_id' => 'required|numeric',
+        'thumbnail' => 'required|file|mimes:jpg,png,webp,svg,jpeg',
+        'body' => 'required',
+    ];
+
     public function index()
     {
-      return view('admin_dashboard.posts.index');
+        return view('admin_dashboard.posts.index', [
+            'posts' => Post::with('category')->orderBy('id', 'DESC')->get(),
+        ]);
     }
 
     public function create()
     {
-      return view('admin_dashboard.posts.create', [
-        'categories' => Category::pluck('name', 'id')
-      ]);
+        return view('admin_dashboard.posts.create', [
+            'categories' => Category::pluck('name', 'id')
+        ]);
     }
     
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate($this->rules);
+        $validated['user_id'] = auth()->id();
+        $post = Post::create($validated);
+
+        if($request->has('thumbnail'))
+        {
+            $thumbnail = $request->file('thumbnail');
+            $filename = $thumbnail->getClientOriginalName();
+            $file_extension = $thumbnail->getClientOriginalExtension();
+            $path = $thumbnail->store('images', 'public');
+
+            $post->image()->create([
+                'name' => $filename,
+                'extension' => $file_extension,
+                'path' => $path
+            ]);
+        }
+
+        $tags = explode(',', $request->input('tags'));
+        $tags_ids = [];
+        foreach($tags as $tag){
+            $tag_ob = Tag::create(['name' => trim($tag)]);
+            $tags_ids[] = $tag_ob->id;
+        }
+        
+        if(count($tags_ids) > 0)
+            $post->tags()->sync( $tags_ids );
+
+        return redirect()->route('admin.posts.create')->with('success', 'Post has been created.');
     }
 
     public function show($id)
     {
         //
+    }
+    
+    public function edit(Post $post)
+    {
+        $tags = '';
+        foreach($post->tags as $key => $tag)
+        {
+            $tags .= $tag->name;
+            if($key !== count($post->tags) - 1)
+                $tags .= ', ';
+        }
+        
+        return view('admin_dashboard.posts.edit', [
+            'post' => $post,
+            'tags' => $tags,
+            'categories' => Category::pluck('name', 'id')
+        ]);
+    }
+    
+    public function update(Request $request, Post $post)
+    {
+        $this->rules['thumbnail'] = 'nullable|file|mimes:jpg,png,webp,svg,jpeg|dimensions:max_width=800,max_height=300';
+        $validated = $request->validate($this->rules);
+        $validated['approved'] = $request->input('approved') !== null;
+
+        $post->update($validated);
+
+        if($request->has('thumbnail'))
+        {
+            $thumbnail = $request->file('thumbnail');
+            $filename = $thumbnail->getClientOriginalName();
+            $file_extension = $thumbnail->getClientOriginalExtension();
+            $path = $thumbnail->store('images', 'public');
+
+            $post->image()->update([
+                'name' => $filename,
+                'extension' => $file_extension,
+                'path' => $path
+            ]);
+        }
+
+        $tags = explode(',', $request->input('tags'));
+        $tags_ids = [];
+        foreach($tags as $tag){
+
+            $tag_exist = $post->tags()->where('name', trim($tag) )->count();
+            if($tag_exist == 0) {
+                $tag_ob = Tag::create(['name' => $tag]);
+                $tags_ids[] = $tag_ob->id;
+            }
+        }
+        
+        if(count($tags_ids) > 0)
+            $post->tags()->syncWithoutDetaching( $tags_ids );
+        
+
+        return redirect()->route('admin.posts.edit', $post)->with('success', 'Post has been updated.');
+    }
+
+    public function destroy(Post $post)
+    {
+        $post->tags()->delete();
+        $post->delete();
+        return redirect()->route('admin.posts.index')->with('success', 'Post has been Deleted.');
     }
 }
--- app/Http/Controllers/AdminControllers/TinyMCEController.php
+++ app/Http/Controllers/AdminControllers/TinyMCEController.php
@@ -10,8 +10,7 @@ class TinyMCEController extends Controller
     public function upload_tinymce_image()
     {
       $file = request()->file('file');
-      $filename = $file->getClientOriginalName();
-      $path = $file->storeAs('tinymce_uploads', $filename, 'public');
+      $path = $file->store('tinymce_uploads', 'public');
       return response()->json(['location' => "/storage/$path"]);
     }
 }
